2eb0efee5ca68e1686149baee757c00ddc2f8850
Xiaochen Li <35388161+SeuperHakkerJa@users.noreply.github.com>
2019-05-21 14:10:02 -0700

    Add files via upload

---
+++ float_int.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main( void ) {
+
+  float f = 13.5625;
+  printf( "the bit config of %f is %x\n",f, * ( int* ) &f );
+
+  int x = 0xC2578000;
+  printf( "the bit config %x represents float %f\n",x, * (float*) &x );
+
+  return 0;
+}
+
+
+
